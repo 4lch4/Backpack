a805f2d96ce18cf24f773ec3f78baf8ad059fac4
Devin W. Leaman <git@4lch4.email>
2023-10-29 15:19:08 -0500

    refactor(ElysAid): moved functions out of ElysAid class
    This commit moves the functions from within the ElysAid class to the top-level as I'll be doing exports differently going forward.

--- src/apis/Elysia.ts
+++ src/apis/Elysia.ts
@@ -1,73 +1,26 @@
 import Table from 'cli-table'
-import { InternalRoute, Elysia as PrimeElysia } from 'elysia'
+import { Elysia, InternalRoute } from 'elysia'
 
-/**
- * A class containing various utility functions for use in APIs developed with [Elysia][0].
- *
- * [0]: https://elysiajs.com
- */
-export class Elysia {
-  public static printElysiaRoutes<T extends PrimeElysia>(app: T): void {
-    const table = new Table({
-      head: ['Path', 'Method'],
-      colAligns: ['left', 'middle'],
-      style: {
-        head: ['cyan'],
-      },
-    })
+export function printRoutes(routes: InternalRoute[]) {
+  const table = new Table({
+    head: ['Path', 'Method'],
+    colAligns: ['left', 'middle'],
+    style: {
+      head: ['cyan'],
+    },
+  })
 
-    for (const route of app.routes) {
-      table.push([route.path, route.method.toUpperCase()])
-    }
-
-    console.log(`\n${table.toString()}\n`)
+  for (const route of routes) {
+    table.push([route.path, route.method.toUpperCase()])
   }
 
-  /**
-   * Prints the given array of Elysia routes to the console as a table.
-   *
-   * @param routes An array of routes from an Elysia instance to print to console.
-   *
-   * @example
-   * ```ts
-   * import { ElysAid } from '@4lch4/backpack'
-   * import { Elysia } from 'elysia'
-   *
-   * const api = new Elysia().get('/test', ctx => 'OK').post('/test', ctx => 'OK')
-   *
-   * ElysAid.printRoutes(api.routes)
-   *
-   * api.listen(4242)
-   *
-   * Output:
-   *
-   * ┌──────────────────────┬────────┐
-   * │ Path                 │ Method │
-   * ├──────────────────────┼────────┤
-   * │ /test                │  GET   │
-   * ├──────────────────────┼────────┤
-   * │ /test                │  POST  │
-   * └──────────────────────┴────────┘
-   *
-   * ```
-   */
-  public static printRoutes(routes: InternalRoute[]) {
-    const table = new Table({
-      head: ['Path', 'Method'],
-      colAligns: ['left', 'middle'],
-      style: {
-        head: ['cyan'],
-      },
-    })
+  console.log(`\n${table.toString()}\n`)
+}
 
-    for (const route of routes) {
-      table.push([route.path, route.method.toUpperCase()])
-    }
+export const healthCheckRoutes = new Elysia()
+  .get('/health', () => 'OK')
+  .get('/readiness', () => 'OK')
 
-    console.log(`\n${table.toString()}\n`)
-  }
-
-  public static healthCheckRoutes(prefix?: string) {
-    return new PrimeElysia({ prefix }).get('/health', () => 'OK').get('/readiness', () => 'OK')
-  }
+export function prefixedHealthCheckRoutes(prefix?: string) {
+  return new Elysia({ prefix }).get('/health', () => 'OK').get('/readiness', () => 'OK')
 }
