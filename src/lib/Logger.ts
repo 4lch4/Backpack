da246bdce2362b793c6a84acd9200455bb99b1bc
Devin W. Leaman <git@4lch4.email>
2023-11-02 21:13:06 -0500

    feat: added `success` method to logger
    This commit adds a new `success` method to the exported logger that will log to the `success` level.

--- src/lib/Logger.ts
+++ src/lib/Logger.ts
@@ -8,19 +8,37 @@ import {
   DEFAULT_LOG_SERVICE,
 } from '@constants/index.js'
 import { arch, platform } from 'os'
-import Winston, { transports as WinstonTransports } from 'winston'
+import Winston, {
+  LeveledLogMethod,
+  Logger as WinstonLogger,
+  transports as WinstonTransports,
+} from 'winston'
 
-export const ConsoleTransport = new WinstonTransports.Console({
+/**
+ * A Winston Transport that logs to console, with colors for success and info.
+ */
+const ConsoleTransport = new WinstonTransports.Console({
   format: Winston.format.combine(
     Winston.format.colorize({ colors: { success: 'green', info: 'cyan' } }),
     Winston.format.simple()
   ),
 })
 
-export function AxiomTransport(dataset: string, token: string, orgId: string) {
+/**
+ * Creates a new Winston transport that logs to Axiom using the given dataset, token, and org ID,
+ * and returns it.
+ *
+ * @param dataset The Axiom Dataset to log to.
+ * @param token The Axiom Token to use for authentication.
+ * @param orgId The Axiom Org ID to use for logging.
+ *
+ * @returns A new Winston transport that logs to Axiom.
+ */
+function AxiomTransport(dataset: string, token: string, orgId: string) {
   return new WinstonTransport({ dataset, token, orgId })
 }
 
+// Initialize the transports array with the console transport.
 const transports: Winston.transport[] = [ConsoleTransport]
 
 // Ensure that we only log to Axiom if all required environment variables are set.
@@ -28,7 +46,18 @@ if (AxiomDataSet && AxiomOrgId && AxiomToken) {
   transports.push(AxiomTransport(AxiomDataSet, AxiomToken, AxiomOrgId))
 }
 
-export const logger = Winston.createLogger({
+/**
+ * An interface for the logger that extends the Winston logger and adds a `success` method.
+ */
+interface Logger extends WinstonLogger {
+  success: LeveledLogMethod
+}
+
+/**
+ * A Winston logger that handles logging how I, _@4lch4_, prefer it be handled. It logs to the
+ * console and to Axiom if the required environment variables are set.
+ */
+const logger: Logger = Winston.createLogger({
   defaultMeta: {
     service: process.env.LOG_SERVICE || process.env.APP_NAME || DEFAULT_LOG_SERVICE,
     nodeEnv: process.env.NODE_ENV || DEFAULT_LOG_NODE_ENV,
@@ -39,4 +68,8 @@ export const logger = Winston.createLogger({
   level: process.env.LOG_LEVEL || process.env.NODE_ENV === 'production' ? 'info' : 'debug',
   levels: { ...Winston.config.npm.levels, success: -1 },
   transports,
-})
+}) as Logger
+
+logger.success = logger.log.bind(logger, 'success')
+
+export { AxiomTransport, ConsoleTransport, logger }
